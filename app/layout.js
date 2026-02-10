67f662a64126bfa7ec49bc8478d33c7f214c6142
sachin gupta <guptasachin@hello.in>
2026-02-10 10:34:42 +0530

    updated

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,20 @@
+// import "./globals.css";
+// export default function RootLayout({ children }) {
+//   return (
+//     <html lang="en">
+//       <body>{children}</body>
+//     </html>
+//   );
+// }
+
 import "./globals.css";
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
       <body>{children}</body>
     </html>
   );
