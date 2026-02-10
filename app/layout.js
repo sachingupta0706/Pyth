14ed7085eedf4057c1ce53da4cc18c5a22130e74
sachin gupta <guptasachin@hello.in>
2026-02-10 18:12:07 +0530

    first commit

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,13 @@
+
+
 // import "./globals.css";
+
 // export default function RootLayout({ children }) {
 //   return (
 //     <html lang="en">
+//       <head>
+//         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+//       </head>
 //       <body>{children}</body>
 //     </html>
 //   );
@@ -9,12 +15,20 @@
 
 import "./globals.css";
 
+export const viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 5,
+};
+
+export const metadata = {
+  title: "Confident - Dental Care Clinic",
+  description: "High Quality & Painless Dentistry",
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      </head>
       <body>{children}</body>
     </html>
   );
